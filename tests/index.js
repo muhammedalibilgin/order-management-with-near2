02510e61a45be1798515fc3854b9dcc64bdc38ac
muhammedalibilgin <bilginmuhammedali@gmail.com>
2022-05-15 12:12:53 +0300

    some change

--- tests/index.js
+++ tests/index.js
@@ -1,4 +1,8 @@
 import assert from "assert";
-import { add } from "../build/debug.js";
+import { add, multi } from "../build/debug.js";
+
 assert.strictEqual(add(1, 2), 3);
 console.log("ok");
+
+assert.strictEqual(multi(3, 3), 9);
+console.log("okey");
